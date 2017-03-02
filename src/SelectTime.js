0ecec66400c39008fafb99d6872bad9cd64fc2b5
Владос <VladislavBoldyr>
2017-03-03 00:38:24 +0600

    inverse image

--- src/SelectTime.js
+++ src/SelectTime.js
@@ -1,17 +1,32 @@
 import React,{Component} from 'react';
-import playButton from './playButtonDown.svg';
+import playButtonDown from './playButtonDown.svg';
+import playButtonUp from './playButtonUp.svg';
 import './SelectTime.css';
 
 export  default class SelectTime extends React.Component {
+  constructor(props) {
+     super(props);
+     this.state={
+     checkImage:false
+     }
+     this.inverseImage = this.inverseImage.bind(this);
+  }
+  inverseImage() {
+    this.setState({
+      checkImage:!this.state.checkImage
+    });
+  }
   render () {
+    let checkViewImage = <input type="image" name="picture"  src={playButtonDown} className="play" onClick={this.inverseImage} />
+    if (this.state.checkImage) {
+      checkViewImage = <input type="image" name="picture"  src={playButtonUp} className="play" onClick={this.inverseImage} />
+    }
     return (
           <div>
             <div>
-              <input type="image" name="picture"  src={playButton} className="play" /> 
-
-
-
+            {checkViewImage}
             </div>
+
 
 
           </div>
